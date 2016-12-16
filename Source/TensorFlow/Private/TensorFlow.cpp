f1956f683a08cd20fe0b8efe21682fa48b6fdc75
getnamo <getnamo@gmail.com>
2016-12-16 14:22:36 +0000

    basic attempt at dll bind

--- Source/TensorFlow/Private/TensorFlow.cpp
+++ Source/TensorFlow/Private/TensorFlow.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #include "TensorFlowPrivatePCH.h"
+#include "c_api.h"
 
 #define LOCTEXT_NAMESPACE "FTensorFlowModule"
 
--- Source/TensorFlow/TensorFlow.Build.cs
+++ Source/TensorFlow/TensorFlow.Build.cs
@@ -1,13 +1,53 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
+
 
 public class TensorFlow : ModuleRules
 {
-	public TensorFlow(TargetInfo Target)
+    private string BinariesPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../Binaries/")); }
+    }
+
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
+    }
+
+    private string TensorflowThirdParty
+    {
+        get { return Path.GetFullPath(Path.Combine(ThirdPartyPath, "Tensorflow")); }
+    }
+
+    public bool LoadLib(TargetInfo Target)
+    {
+        bool isLibrarySupported = false;
+
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
+            string TensorLibPath = Path.Combine(TensorflowThirdParty, "Lib");
+
+            //disable all this for now
+            //PublicDelayLoadDLLs.Add("pywrap_tensorflow.dll");
+            //RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(BinariesPath, "Win64", "pywrap_tensorflow.dll")));
+
+            //doesn't load right for some reason =/
+            //PublicAdditionalLibraries.Add(Path.Combine(TensorLibPath, PlatformString, "pywrap_tensorflow.lib"));
+        }
+
+        return isLibrarySupported;
+    }
+
+
+    public TensorFlow(TargetInfo Target)
 	{
-		
-		PublicIncludePaths.AddRange(
+
+    PublicIncludePaths.AddRange(
 			new string[] {
 				"TensorFlow/Public"
 				// ... add public include paths required here ...
@@ -18,6 +58,7 @@ public class TensorFlow : ModuleRules
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				"TensorFlow/Private",
+                Path.Combine(TensorflowThirdParty, "Include"),
 				// ... add other private include paths required here ...
 			}
 			);
@@ -50,5 +91,7 @@ public class TensorFlow : ModuleRules
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
-	}
+
+        LoadLib(Target);
+    }
 }
